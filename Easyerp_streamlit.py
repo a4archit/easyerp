86e995ddf158f427a9221e6eef6ad9020136cf7a
Archit <archittyagi599@gmail.com>
2024-11-17 19:57:46 +0530

    Update Easyerp_streamlit.py

--- Easyerp_streamlit.py
+++ Easyerp_streamlit.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from bs4 import BeautifulSoup
 import streamlit as st 
